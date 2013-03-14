ce1bab98ce1945dff5f7c70aa28686f547dcb114
Brenda McIntosh <brenda.k.mcintosh@gmail.com>
2013-03-13 21:04:47 -0700

    Positioned image field mid left

--- main.elm
+++ main.elm
@@ -2,12 +2,14 @@ import Input
 import Graphics
 import Signal 
 
-main = lift2 beside firstImageFieldSignal slideshow
+main = lift2 beside sidebar slideshow
 
 (firstImageField, input) = textField "First Image"
 
 firstImageFieldSignal = constant firstImageField
 
 slideshow = images input
+
+sidebar = lift4 container (constant (widthOf firstImageField)) Window.height (constant midLeft) firstImageFieldSignal
 
 -- http://www.adogabouttown.com/wp-content/uploads/2008/08/american-crow.jpg
