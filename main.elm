22214df29552d3f7a4286e9744317a642f6f2048
Brenda McIntosh <brenda.k.mcintosh@gmail.com>
2013-03-13 21:11:51 -0700

    Centered the slideshow
    (of crows...)

--- main.elm
+++ main.elm
@@ -2,13 +2,15 @@ import Input
 import Graphics
 import Signal 
 
-main = lift2 beside sidebar slideshow
+main = lift2 beside sidebar slideshowContainer
 
 (firstImageField, input) = textField "First Image"
 
 firstImageFieldSignal = constant firstImageField
 
 slideshow = images input
+
+slideshowContainer = lift4 container (lift widthOf slideshow) Window.height (constant middle) slideshow
 
 sidebar = lift4 container (constant (widthOf firstImageField)) Window.height (constant midLeft) firstImageFieldSignal
 
