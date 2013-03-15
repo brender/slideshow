f81c3b99dacf5fea8fdc1b0dc7ca9c81ab873267
Brenda McIntosh <brenda.k.mcintosh@gmail.com>
2013-03-14 20:31:05 -0700

    Added a third image field

--- main.elm
+++ main.elm
@@ -3,27 +3,30 @@ import Graphics
 import Signal
 import Time
 
-(firstImageField, input1) = textField "First Image"
-(secondImageField, input2) = textField "Second Image"
+(firstImageField, input1) = textField "First Image URL"
+(secondImageField, input2) = textField "Second Image URL"
+(thirdImageField, input3) = textField "Third Image URL"
 
 main = lift2 beside sidebar slideshowContainer
 
-imageFieldsSignal = constant (flow down [ firstImageField, secondImageField ])
+imageFieldsSignal = constant (flow down [ firstImageField, secondImageField, thirdImageField ])
 
-slideshow = images (switchEvery input1 input2 second)
+slideshow = images (switchEvery input1 input2 input3 (second / 4))
 
 slideshowContainer = lift4 container (lift widthOf slideshow) Window.height (constant middle) slideshow
 
 sidebar = lift4 container (constant (widthOf firstImageField)) Window.height (constant midLeft) imageFieldsSignal
 
--- switchEvery :: Signal a -> Signal a -> Time -> Signal a
+-- switchEvery :: Signal a -> Signal a -> Signal a -> Time -> Signal a
 --      Switches between the two signals every 't' seconds.
 --
--- (switchEvery input1 input2 (second / 2))
-switchEvery a b t =
-    let ta = every (t * 2)
+-- (switchEvery input1 input2 input3 (second / 2))
+switchEvery a b c t =
+    let ta = every (t * 3)
         tb = delay t ta
-    in merge (sampleOn ta a) (sampleOn tb b)
+        tc = delay t tb 
+    in merges [ (sampleOn ta a), (sampleOn tb b), (sampleOn tc c) ]
 
 -- http://www.adogabouttown.com/wp-content/uploads/2008/08/american-crow.jpg
 -- http://brender.github.com/crows/crow1.jpg
+-- http://www.britannica.com/blogs/wp-content/uploads/2009/07/crow1.jpg
