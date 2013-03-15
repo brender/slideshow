e18b2b874183d42bd3fc0d770aacacb9d8d61c82
Brenda McIntosh <brenda.k.mcintosh@gmail.com>
2013-03-14 20:45:46 -0700

    Added default crow images if user input is blank

--- main.elm
+++ main.elm
@@ -7,11 +7,17 @@ import Time
 (secondImageField, input2) = textField "Second Image URL"
 (thirdImageField, input3) = textField "Third Image URL"
 
+
+
 main = lift2 beside sidebar slideshowContainer
 
 imageFieldsSignal = constant (flow down [ firstImageField, secondImageField, thirdImageField ])
 
-slideshow = images (switchEvery input1 input2 input3 (second / 4))
+maybeString1 = lift2 maybeString input1 (constant "http://www.adogabouttown.com/wp-content/uploads/2008/08/american-crow.jpg") 
+maybeString2 = lift2 maybeString input2 (constant "http://brender.github.com/crows/crow1.jpg")
+maybeString3 = lift2 maybeString input3 (constant "http://www.britannica.com/blogs/wp-content/uploads/2009/07/crow1.jpg")
+
+slideshow = images (switchEvery maybeString1 maybeString2 maybeString3 (second / 4))
 
 slideshowContainer = lift4 container (lift widthOf slideshow) Window.height (constant middle) slideshow
 
@@ -26,6 +32,11 @@ switchEvery a b c t =
         tb = delay t ta
         tc = delay t tb 
     in merges [ (sampleOn ta a), (sampleOn tb b), (sampleOn tc c) ]
+
+-- maybeString :: String -> String -> String
+--      Returns the first string, unless it's empty, in which case the default is returned.
+maybeString s d =
+    if s == "" then d else s 
 
 -- http://www.adogabouttown.com/wp-content/uploads/2008/08/american-crow.jpg
 -- http://brender.github.com/crows/crow1.jpg
